43726b9df1498821a533337f34b466bb3912eb4d
googlr <googlr@users.noreply.github.com>
2018-08-02 21:43:16 -0400

    add dynamoDB CRUD functions, test succeed

--- src/main/java/com/jobalert/domain/JobAlert.java
+++ src/main/java/com/jobalert/domain/JobAlert.java
@@ -4,50 +4,127 @@
 package com.jobalert.domain;
 
 import java.util.Arrays;
+import java.lang.Exception;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.TableCollection;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
 
 public class JobAlert {
-    private void createTables(){
-        
-    }
+    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    static DynamoDB dynamoDB = new DynamoDB(client);
+
+    static String tableName = "ExampleTable";
 
     public static void main(String[] args) throws Exception {
 
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-2"))
-                .build();
+        createExampleTable();
+        listMyTables();
+        getTableInformation();
+        updateExampleTable();
 
-        DynamoDB dynamoDB = new DynamoDB(client);
+        deleteExampleTable();
+    }
 
-        String tableName = "Movies";
+    static void createExampleTable() {
 
         try {
-            System.out.println("Attempting to create table; please wait...");
-            Table table = dynamoDB.createTable(tableName,
-                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
-                            // key
-                            new KeySchemaElement("title", KeyType.RANGE)), // Sort key
-                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
-                            new AttributeDefinition("title", ScalarAttributeType.S)),
-                    new ProvisionedThroughput(10L, 10L));
+
+            List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+            attributeDefinitions.add(new AttributeDefinition().withAttributeName("Id").withAttributeType("N"));
+
+            List<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+            keySchema.add(new KeySchemaElement().withAttributeName("Id").withKeyType(KeyType.HASH)); // Partition
+            // key
+
+            CreateTableRequest request = new CreateTableRequest().withTableName(tableName).withKeySchema(keySchema)
+                    .withAttributeDefinitions(attributeDefinitions).withProvisionedThroughput(
+                            new ProvisionedThroughput().withReadCapacityUnits(5L).withWriteCapacityUnits(6L));
+
+            System.out.println("Issuing CreateTable request for " + tableName);
+            Table table = dynamoDB.createTable(request);
+
+            System.out.println("Waiting for " + tableName + " to be created...this may take a while...");
             table.waitForActive();
-            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+            getTableInformation();
 
         }
         catch (Exception e) {
-            System.err.println("Unable to create table: ");
+            System.err.println("CreateTable request failed for " + tableName);
             System.err.println(e.getMessage());
         }
 
+    }
+
+    static void listMyTables() {
+
+        TableCollection<ListTablesResult> tables = dynamoDB.listTables();
+        Iterator<Table> iterator = tables.iterator();
+
+        System.out.println("Listing table names");
+
+        while (iterator.hasNext()) {
+            Table table = iterator.next();
+            System.out.println(table.getTableName());
+        }
+    }
+
+    static void getTableInformation() {
+
+        System.out.println("Describing " + tableName);
+
+        TableDescription tableDescription = dynamoDB.getTable(tableName).describe();
+        System.out.format(
+                "Name: %s:\n" + "Status: %s \n" + "Provisioned Throughput (read capacity units/sec): %d \n"
+                        + "Provisioned Throughput (write capacity units/sec): %d \n",
+                tableDescription.getTableName(), tableDescription.getTableStatus(),
+                tableDescription.getProvisionedThroughput().getReadCapacityUnits(),
+                tableDescription.getProvisionedThroughput().getWriteCapacityUnits());
+    }
+
+    static void updateExampleTable() {
+
+        Table table = dynamoDB.getTable(tableName);
+        System.out.println("Modifying provisioned throughput for " + tableName);
+
+        try {
+            table.updateTable(new ProvisionedThroughput().withReadCapacityUnits(6L).withWriteCapacityUnits(7L));
+
+            table.waitForActive();
+        }
+        catch (Exception e) {
+            System.err.println("UpdateTable request failed for " + tableName);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    static void deleteExampleTable() {
+
+        Table table = dynamoDB.getTable(tableName);
+        try {
+            System.out.println("Issuing DeleteTable request for " + tableName);
+            table.delete();
+
+            System.out.println("Waiting for " + tableName + " to be deleted...this may take a while...");
+
+            table.waitForDelete();
+        }
+        catch (Exception e) {
+            System.err.println("DeleteTable request failed for " + tableName);
+            System.err.println(e.getMessage());
+        }
     }
 }
